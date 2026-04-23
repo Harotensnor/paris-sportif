dc100e978a45bc0e491a94cbf7f0bf6c9147a25f
Harotensnor <theoboulnois@gmail.com>
2026-04-23 15:39:14 +0200

    v23 refonte debutant: nav passe de 12 a 6 onglets + page Buteurs (Over/BTTS foot) + FAB Que parier maintenant + vocab simplifie (Elo->Force, H2H->Face-a-face, Fiabilite->Confiance, ROI->Rentabilite) + bibliotheque lecons apprises + tooltips pedagogique ? + banniere debutant + libelles confiance en mots - SW v23

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //   * icônes / manifest : cache-first (change rarement).
 //   * tout le reste    : passthrough réseau.
 
-const CACHE_VERSION = 'paris-sportif-v22-2026-04-23-v22.2-statuses-dict-fix';
+const CACHE_VERSION = 'paris-sportif-v23-2026-04-23-v23-refonte-debutant-nav6';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
